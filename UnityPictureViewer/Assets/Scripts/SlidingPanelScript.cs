fe62f02f05e553d4a934538cbdde55cc88a95d1b
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-12-17 15:14:51 +0900

    パネル化の検討だけしておく

---
+++ UnityPictureViewer/Assets/Scripts/SlidingPanelScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingPanelScript : MonoBehaviour
+{
+    RectTransform rts;
+
+    [SerializeField]
+    bool isSliding = false;
+
+    void Start()
+    {
+        rts = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSliding)
+        {
+
+        }
+    }
+}
